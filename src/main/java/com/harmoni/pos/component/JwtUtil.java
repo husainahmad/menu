112124ba95bb004271ee421230bd4b1aa48fe4cf
husainharmoni <husainahmad@gmail.com>
2025-02-08 01:03:06 +0700

    [Menu] now only allow with jwt

---
+++ src/main/java/com/harmoni/pos/component/JwtUtil.java
@@ -0,0 +1,59 @@
+package com.harmoni.pos.component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Getter
+@Component
+public class JwtUtil {
+
+    private static final long EXPIRATION_TIME = 86400000; // 1 day
+
+    @Value("${harmoni.menu.jwt.secret}")
+    private String secretKey;
+
+    @Value("${harmoni.menu.jwt.expired.time}")
+    private long expiredTime;
+
+    public String generateToken(String username) {
+        Instant now = Instant.now();
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusMillis(expiredTime)))
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+}
+
